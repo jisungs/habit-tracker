1a218bd1ab38c6fe514011ad6a0a75793303996f
Jisung Smart <jisung.smart@gmail.com>
2023-05-23 15:56:19 +0900

    add mail verification code function

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,16 @@ from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
+
+# Verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+
 # Create your views here.
 def policy(request):
     return render(request, 'accounts/policy.html')
@@ -21,8 +31,21 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            #USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = '안녕하세요 리프아일랜드 입니다. 회원가입을 완료 해주세요.'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
             messages.success(request, '회원등록이 성공 했습니다.')
-            return redirect('register')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
     context = {
@@ -51,3 +74,20 @@ def logout(request):
     auth.logout(request)
     messages.success(request, '로그아웃을 완료 했습니다.')
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, '반갑습니다. 가입이 완료 되었습니다.')
+        return redirect('login')
+    else:
+        messages.error(request, '활성 코드가 잘못 되었습니다.')
+        return redirect('register')
