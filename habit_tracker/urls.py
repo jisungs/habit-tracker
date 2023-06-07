4915378d7d6c94f68407a8729d701e5f8c4c4590
Jisung Smart <jisung.smart@gmail.com>
2023-06-07 15:02:23 +0900

    Add edit function in the habit-traker app

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -3,7 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.habits_list, name='habits_list'),
-    path('habit_detail/', views.habit_detail, name='habit_detail'),
 
-    path('habit_detail/addGoal/', views.add_goal, name ='add_goal')
+    path('habit_detail/', views.habit_detail, name='habit_detail'),
+    # add goals 
+    path('habit_detail/addGoal/', views.add_goal, name ='add_goal'),
+    #completed the goals 
+    path('habit_detail/completed/<int:pk>', views.completed, name ='completed'),
+    # undo the goals 
+    path('habit_detail/undo_goal/<int:pk>', views.undo_goal, name ='undo_goal'),
+    # Edit goal
+    path('habit_detail/edit_goal/<int:pk>', views.edit_goal, name ='edit_goal'),
+
 ]
--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from .models import Goal
 
 # Create your views here.
@@ -10,8 +10,10 @@ def habits_list(request):
 def habit_detail(request):
     goals = Goal.objects.filter(is_completed=False).order_by('-updated_at')
     
+    completed_goals = Goal.objects.filter(is_completed=True)
     context = {
         'goals': goals,
+        'completed_goals':completed_goals,
     }
 
     return render(request, 'habit_tracker/habit_detail.html', context)
@@ -21,3 +23,18 @@ def add_goal(request):
     goal = request.POST['goal']
     Goal.objects.create(goal=goal)
     return redirect('habit_detail')
+
+def completed(request, pk):
+    goal = get_object_or_404(Goal, pk=pk)
+    goal.is_completed = True
+    goal.save()
+    return redirect('habit_detail')
+
+def undo_goal(request, pk):
+    goal = get_object_or_404(Goal, pk=pk)
+    goal.is_completed = False
+    goal.save()
+    return redirect('habit_detail')
+
+def edit_goal(request, pk):
+    return HttpResponse('Edit button is working')
